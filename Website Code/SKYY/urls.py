2a6fdf3a2d454a324bfc6d1ea0463086ec75418d
Shreyash Salunke <shreyashsalunkesds@gmail.com>
2023-03-03 13:55:31 +0530

    Code Updated

--- Website Code/SKYY/urls.py
+++ Website Code/SKYY/urls.py
@@ -1,4 +1,4 @@
-"""hackstreet URL Configuration
+"""SKYY URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from hackstreet import views
+from SKYY import views
 from ML_Prediction.views import flights
 
 
--- Website Code/SKYY/wsgi.py
+++ Website Code/SKYY/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hackstreet project.
+WSGI config for SKYY project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackstreet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SKYY.settings')
 
 application = get_wsgi_application()
